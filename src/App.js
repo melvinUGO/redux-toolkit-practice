c62bb18f702021c71776a94ae766c1657f101a94
melvinUGO <ugochukwumelvin@gmail.com>
2022-10-23 14:02:16 -0700

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import React from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  React.useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,10 +1,46 @@
 import React from "react";
-import cartItem from "./cartItem";
-import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, amount, total } = useSelector((store) => store.cart);
-  return <div>cartContainer</div>;
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">empty cart</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(openModal())}>
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
 };
 
 export default CartContainer;
